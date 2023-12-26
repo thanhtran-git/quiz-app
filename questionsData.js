7e23b73167f54b99196461cff8b80a4cdfb3fb03
Thanh Tran <85113171+thanhtran-git@users.noreply.github.com>
2023-12-26 20:16:10 +0100

    update

---
+++ questionsData.js
@@ -0,0 +1,66 @@
+export const questions = [
+  {
+    question: "What is the capital of Canada?",
+    answers: [
+      { text: "Toronto", correct: false },
+      { text: "Ottawa", correct: true },
+      { text: "Montreal", correct: false },
+      { text: "Quebec", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital of New Zealand?",
+    answers: [
+      { text: "Wellington", correct: true },
+      { text: "Christchurch", correct: false },
+      { text: "Auckland", correct: false },
+      { text: "Hamilton", correct: false },
+    ],
+  },
+
+  {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Berlin", correct: false },
+      { text: "Paris", correct: true },
+      { text: "Madrid", correct: false },
+      { text: "Rome", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital of Japan?",
+    answers: [
+      { text: "Beijing", correct: false },
+      { text: "Tokyo", correct: true },
+      { text: "Seoul", correct: false },
+      { text: "Bangkok", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital of Brazil?",
+    answers: [
+      { text: "Brasília", correct: true },
+      { text: "Buenos Aires", correct: false },
+      { text: "Rio de Janeiro", correct: false },
+      { text: "Lima", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital of Australia?",
+    answers: [
+      { text: "Sydney", correct: false },
+      { text: "Canberra", correct: true },
+      { text: "Melbourne", correct: false },
+      { text: "Wellington", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital of South Africa?",
+    answers: [
+      { text: "Nairobi", correct: false },
+      { text: "Pretoria", correct: false },
+      { text: "Johannesburg", correct: false },
+      { text: "Cape Town", correct: true },
+    ],
+  },
+];
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { questions } from "./questionsData.js";
+
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
@@ -6,22 +8,23 @@ const questionIndex = document.getElementById("question-index");
 const answerButtonsElement = document.getElementById(
   "answer-buttons-container"
 );
-const showAnswerButton = document.getElementById("show-answer-button");
+const scoreContainer = document.getElementById("score-container");
+const scoreNumber = document.getElementById("score");
 
-let currentQuestionIndex = 0;
-let correctAnswers
+let currentQuestionIndex, correctAnswers;
 
 startButton.addEventListener("click", () => {
-  startGame();
+  startQuiz();
 });
 nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
   setNextQuestion();
 });
 
-function startGame() {
+function startQuiz() {
   startButton.classList.add("hide");
-
+  scoreContainer.classList.add("hide");
+  correctAnswers = 0;
   currentQuestionIndex = 0;
   questionContainerElement.classList.remove("hide");
   setNextQuestion();
@@ -42,7 +45,6 @@ function showQuestion(question) {
     button.classList.add("answer-btn");
     if (answer.correct) {
       button.dataset.correct = answer.correct;
-      correctAnswers++
     }
     button.addEventListener("click", selectAnswer);
     answerButtonsElement.appendChild(button);
@@ -58,17 +60,25 @@ function resetState() {
 }
 
 function selectAnswer(e) {
+  scoreContainer.classList.remove("hide");
   const selectedButton = e.target;
   const correct = selectedButton.dataset.correct;
+
   setStatusClass(document.body, correct);
   Array.from(answerButtonsElement.children).forEach((button) => {
     setStatusClass(button, button.dataset.correct);
   });
+
+  if (correct) {
+    correctAnswers++;
+  }
+  scoreNumber.innerText = `Correct Answers: ${correctAnswers} / ${questions.length}`;
+
   if (questions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove("hide");
   } else {
-    startButton.innerText = "Restart the Quiz";
-    startButton.style.backgroundColor = "red";
+    startButton.innerText = "Restart Quiz";
+    // startButton.style.backgroundColor = "red";
     startButton.classList.remove("hide");
   }
 }
@@ -86,70 +96,3 @@ function clearStatusClass(element) {
   element.classList.remove("correct");
   element.classList.remove("wrong");
 }
-
-const questions = [
-  {
-    question: "What is the capital of Canada?",
-    answers: [
-      { text: "Toronto", correct: false },
-      { text: "Ottawa", correct: true },
-      { text: "Montreal", correct: false },
-      { text: "Quebec", correct: false },
-    ],
-  },
-  {
-    question: "What is the capital of New Zealand?",
-    answers: [
-      { text: "Wellington", correct: true },
-      { text: "Christchurch", correct: false },
-      { text: "Auckland", correct: false },
-      { text: "Hamilton", correct: false },
-    ],
-  },
-
-  {
-    question: "What is the capital of France?",
-    answers: [
-      { text: "Berlin", correct: false },
-      { text: "Paris", correct: true },
-      { text: "Madrid", correct: false },
-      { text: "Rome", correct: false },
-    ],
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Beijing", correct: false },
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Bangkok", correct: false },
-    ],
-  },
-  {
-    question: "What is the capital of Brazil?",
-    answers: [
-      { text: "Brasília", correct: true },
-      { text: "Buenos Aires", correct: false },
-      { text: "Rio de Janeiro", correct: false },
-      { text: "Lima", correct: false },
-    ],
-  },
-  {
-    question: "What is the capital of Australia?",
-    answers: [
-      { text: "Sydney", correct: false },
-      { text: "Canberra", correct: true },
-      { text: "Melbourne", correct: false },
-      { text: "Wellington", correct: false },
-    ],
-  },
-  {
-    question: "What is the capital of South Africa?",
-    answers: [
-      { text: "Nairobi", correct: false },
-      { text: "Pretoria", correct: false },
-      { text: "Johannesburg", correct: false },
-      { text: "Cape Town", correct: true },
-    ],
-  },
-];
